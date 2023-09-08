2372e5978f614369e9afa8cfba1e85aa7c0b0d23
Oscar Espitia <odespitia@sena.edu.co>
2023-09-08 13:57:42 -0500

    init

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,7 +5,164 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Titulo de primer nivel
+      h1 Empaque y embalaje de huevos de gallina
+
+    p.mb-4(data-aos="fade-right") Para entender estos dos procesos hay que tener en cuenta los conceptos explícitos de cada uno de esos términos, los cuales permiten determinar en qué consiste y cómo se diferencian de acuerdo con sus características:
+
+    h4.text-center Empaque y embalaje de huevos de gallina
+
+    .row.justify-content-center.align-items-center
+      .col-lg-10.col-12.mb-5(data-aos="fade-down")
+        ImagenInfografica.color-acento-botones.mb-5(data-aos="fade-down")
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/temas/tema4/tema4-info1.svg', alt='')
+          .tarjeta.color-primario.basico.p-3(x="15%" y="25%" numero="1")
+            h4 Empaque primario
+            p Es el recipiente destinado a contener un producto y que entra en contacto con el mismo.
+          .tarjeta.color-primario.basico.p-3(x="58%" y="75%" numero="2")
+            h4 Empaque secundario
+            p Es el recipiente encargado de contener el empaque primario.
+          .tarjeta.color-primario.basico.p-3(x="10%" y="72%" numero="3")
+            h4 Embalaje
+            p Son todos los materiales, procedimientos y métodos que sirven para acondicionar, presentar, manipular, almacenar, conservar y transportar una mercancía. 
+
+    p.mb-4(data-aos="fade-right") Los huevos deben cumplir con la normatividad, legislación y reglamentos aplicables para su comercialización y exportación/importación en los cuales se definen los criterios de calidad, peso, frescura, envasado y etiquetado. 
+
+
+    .row.justify-content-center.align-items-center.mb-5.tarjeta.color-acento-botones.p-4(data-aos="fade-down") 
+      .col-md-4.col-12
+        img(src='@/assets/curso/temas/tema4/tema4-1.png' alt='')
+      .col-md-8.col-12
+          p.mb-0 Una vez que el huevo ha sido seleccionado y clasificado, se empaca, en embalajes nuevos y resistentes a golpes, estos deben estar secos, limpios y en buen estado. Normalmente están fabricados con materiales que protegen a los huevos de olores extraños y de posibles alteraciones de la calidad (pulpa moldeada, cartón o polietileno).
+
+    p.mb-4(data-aos="fade-right") Es así como se puede determinar qué tipo de empaque y embalaje de acuerdo con su destino final de comercialización, siendo las siguientes las dos opciones más usadas:
+
+
+    .row.justify-content-center.align-items-center.mb-4.con-borde
+      .col-12
+        .tarjeta-avatar-b.mb-4(data-aos="fade-down")
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema4/tema4-2.svg' alt='')
+          .tarjeta.color-primario--borde
+            .p-4
+              p.mb-0 #[b Huevo para plato:] este debe venderse al consumidor lo antes posible dentro de los 21 días posteriores a la fecha de puesta, aunque se puede consumir hasta la fecha de consumo preferente indicada en el empaque o envase, que es 28 días desde el día de la puesta. Los huevos que están destinados a este fin deben cumplir con la normatividad aplicada para comercialización, exportación o importación en donde se exponen los criterios de peso, frescura, envasado, etiquetado y calidad, el proceso del empaque toma lugar una vez se ha realizado la selección clasificación. 
+        .tarjeta-avatar-b.mb-4(data-aos="fade-down")
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema4/tema4-3.svg' alt='')
+          .tarjeta.color-acento-contenido--borde
+            .p-4
+              p.mb-0 #[b Para venta a granel o mayoreo:] estos se destinarán a la venta a granel o mayoreo y se ponen en conos, cajas o charolas en las que se puedan sujetar sin moverse, ya sean de polietileno, cartón o pulpa moldeada, normalmente vienen contenidos en empaques de 24 o 30 piezas según lo maneje el proveedor, y estas a su vez se empacan o se contienen en cajas de cartón que las protege de cualquier golpe. Las cajas se sellan, se sujetan con cinta adhesiva y se les adhiere la etiqueta por fuera con la información que las identifique plenamente. La resistencia de las cajas debe ser capaz de soportar la altura del apilamiento sin que estas se deformen.
+
+    Separador
+    #t_4_1.titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 4.1 Tipos de empaque y embalaje
+
+    p.mb-3(data-aos="fade-right") Estos juegan un papel muy importante en la conservación del producto, ya que necesita contar con ciertas características para que su caducidad no llegue tan pronto, pueden ser de diferentes materiales, por ejemplo: plástico, cartón o pulpa de cartón.
+    p.mb-5(data-aos="fade-right") El empaque para huevo es conocido como cartón o maple para huevos y estos se encargan de darle protección, temperatura ideal y la cantidad exacta de luz a una cantidad específica de huevos, además, los protege de ciertas partículas ambientales y pueden ser de diferentes tamaños, dependiendo del número de huevos que se desee empacar. Las características son las siguientes:
+
+    .row.justify-content-center.align-items-center.mb-5.tarjeta.color-acento-contenido.p-4(data-aos="fade-down") 
+      .col-md-8.col-12
+          p.texto-negro Bandejas de cartón presentan un color uniforme, son fabricadas con material reciclable mediante un proceso de moldeo. Estos empaques no deben presentar manchas, deben estar libres de daños en los alvéolos, así como las pestañas deben ser uniformes y bien definidas.
+          p.texto-negro Bandejas de plástico presentan un color transparente, son fabricadas con plástico reciclado (R-PET) mediante proceso de moldeo de láminas de PET (polietileno tereftalato). Estos empaques no deben presentar perforaciones, cambios en tamaños o calibre y diferencias de color, así como las pestañas/botones de cierre del empaque deben ser uniformes y bien definidos.
+          p.texto-negro.mb-0 A continuación, se presentan diferentes tipos de empaque o embalaje, su descripción e imagen de referencia:
+      .col-md-4.col-12
+        img(src='@/assets/curso/temas/tema4/tema4-4.png' alt='')
+
+    .row.justify-content-center.align-items-center
+      .col-lg-10.col-12.mb-0(data-aos="fade-down")
+        ImagenInfograficaB.color-acento-botones.mb-5
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/temas/tema4/tema4-info2.svg', alt='')
+          .tarjeta.color-primario.basico.p-3(x="50%" y="10%" tooltip="" numero="1")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-7.png', alt='')
+              .col-md-7.col-12
+                p Bandejas de cartón por 30 unidades: en esta presentación se comercializa el huevo AAA, AA, A y B.
+          .tarjeta.color-primario.basico.p-3(x="75%" y="18.5%" tooltip="" numero="2")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-8.png', alt='')
+              .col-md-7.col-12
+                p Bandejas de cartón por 20 unidades: en esta presentación se comercializa el huevo jumbo.
+          .tarjeta.color-primario.basico.p-3(x="84.5%" y="34.5%" tooltip="" numero="3")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-9.png', alt='')
+              .col-md-7.col-12
+                p Bandejas de cartón por 15 unidades: en esta presentación se comercializa el huevo AA y A.
+          .tarjeta.color-primario.basico.p-3(x="84.5%" y="51.5%" tooltip="" numero="4")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-10.png', alt='')
+              .col-md-7.col-12
+                p Bandejas de cartón por 12 unidades: en esta presentación se comercializa el huevo AA y A.
+          .tarjeta.color-primario.basico.p-3(x="74%" y="68.5%" tooltip="" numero="5")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-11.png', alt='')
+              .col-md-7.col-12
+                p Bandejas PET por 10 unidades: en esta presentación se comercializa el huevo AA y A.
+          .tarjeta.color-primario.basico.p-3(x="50%" y="74.5%" tooltip="" numero="6")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-12.png', alt='')
+              .col-md-7.col-12
+                p Bandejas PET por 12 unidades: en esta presentación se comercializa el huevo AA y A.
+          .tarjeta.color-primario.basico.p-3(x="25%" y="65.5%" tooltip="" numero="7")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-13.png', alt='')
+              .col-md-7.col-12
+                p Bandejas PET por 24 unidades: en esta presentación se comercializa el huevo AA y A.
+          .tarjeta.color-primario.basico.p-3(x="18%" y="50.5%" tooltip="" numero="8")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-14.png', alt='')
+              .col-md-7.col-12
+                p Bandejas PET por 30 unidades: en esta presentación se comercializa el huevo AA y A.
+          .tarjeta.color-primario.basico.p-3(x="18%" y="34.5%" tooltip="" numero="9")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-15.png', alt='')
+              .col-md-7.col-12
+                p Bandejas PET por 15 unidades: en esta presentación se comercializa el huevo AAA, AA y A.
+          .tarjeta.color-primario.basico.p-3(x="25%" y="18.5%" tooltip="" numero="10")
+            .row.align-items-center
+              .col-md-5.col-12
+                img(src='@/assets/curso/temas/tema4/tema4-16.png', alt='')
+              .col-md-7.col-12
+                p Caja por 60 y 90 unidades: en esta presentación se comercializa el huevo AA y A.
+
+
+    Separador.pt-0
+    #t_4_2.titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 4.2 Parámetros de calidad del empaque y embalaje
+
+    p.mb-5(data-aos="fade-right") El empaque o bandeja más conocido es el de cartón o cubeta, donde en algunos casos cuentan con una cobertura parcial y, en otros, es total, dependiendo de la industria. 
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-down") 
+      .col-12.col-md-11.col-lg-10 
+        .row.tarjeta.color-acento-contenido.p-4.align-items-center
+          .col-md-3.col-12
+            img(src='@/assets/curso/temas/tema4/tema4-5.png' alt='')
+          .col-md-9.col-12
+              p.texto-negro.mb-0 Estas bandejas son elaboradas, en ocasiones, con papel reciclado, de tal manera que se usan una sola vez; es decir, que su vida útil es tan corta que sus propiedades se van perdiendo y, por ende, deben ser nuevas al momento de empacar el producto.
+
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-down") 
+      .col-md-3.col-lg-2.col-12
+        img(src='@/assets/curso/temas/tema4/tema4-6.svg' alt='')
+      .col-md-9.col-lg-10.col-12
+        p Por ello, la Federación Nacional de Avicultores de Colombia (FENAVI), dio a conocer varias características que los consumidores deben tener en cuenta en los empaques al momento de comprar huevos, entre estos se encuentran aspectos como la limpieza, su color uniforme, que no se evidencian daños en los alvéolos, que estén libres de arrugas, manchas, olores y totalmente secos.
+        p  Fenavi, como gremio, indica que una de las características de las bandejas de huevo reutilizadas son el deterioro en los bordes, humedad, residuos de huevo, materia fecal, plumas, polvo, insectos, huevos secos adheridos al cartón y decoloración, lo cual puede poner en riesgo la calidad del producto.
+ 
+
+
+
+
 
 </template>
 
